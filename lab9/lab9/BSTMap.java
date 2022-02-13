f1ac68ec672493c26c4bcd4a1e61373196d22961
jiangtian <3210102488@zju.edu.cn>
2022-02-13 17:02:54 +0800

    My code in the lab9 and the lab10

---
+++ lab9/lab9/BSTMap.java
@@ -0,0 +1,226 @@
+package lab9;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Implementation of interface Map61B with BST as core data structure.
+ *
+ * @author Jiang Tian
+ */
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+
+    private class Node {
+        /* (K, V) pair stored in this Node. */
+        private K key;
+        private V value;
+
+        /* Children of this Node. */
+        private Node left;
+        private Node right;
+
+        private Node(K k, V v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private Node root;  /* Root node of the tree. */
+    private int size; /* The number of key-value pairs in the tree */
+
+    /* Creates an empty BSTMap. */
+    public BSTMap() {
+        this.clear();
+    }
+
+    /* Removes all of the mappings from this map. */
+    @Override
+    public void clear() {
+        root = null;
+        size = 0;
+    }
+
+    /** Returns the value mapped to by KEY in the subtree rooted in P.
+     *  or null if this map contains no mapping for the key.
+     */
+    private V getHelper(K key, Node p) {
+        if(p == null) {
+            return null;
+        }
+        int cmp = key.compareTo(p.key);
+        if(cmp == 0) {
+            return p.value;
+        }
+        if(cmp > 0) {
+            return getHelper(key, p.right);
+        } else {
+            return getHelper(key, p.left);
+        }
+    }
+
+    /** Returns the value to which the specified key is mapped, or null if this
+     *  map contains no mapping for the key.
+     */
+    @Override
+    public V get(K key) {
+        if(this.root == null) {
+            return null;
+        }
+        int cmp = this.root.key.compareTo(key);
+        if(cmp == 0) {
+            return this.root.value;
+        }
+        if(cmp > 0) {
+            return getHelper(key, this.root.left);
+        }else {
+            return getHelper(key, this.root.right);
+        }
+    }
+
+    /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
+      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
+     */
+    private Node putHelper(K key, V value, Node p) {
+        if(p == null) {
+             p = new Node(key, value);
+             p.left = null;
+             p.right = null;
+        }
+        int cmp = key.compareTo(p.key);
+        if(cmp == 0) {
+            p.value = value;
+            return p;
+        } else if(cmp > 0) {
+            p.right = putHelper(key, value, p.right);
+            return p;
+        } else {
+            p.left = putHelper(key, value, p.left);
+            return p;
+        }
+    }
+
+    /** Inserts the key KEY
+     *  If it is already present, updates value to be VALUE.
+     */
+    @Override
+    public void put(K key, V value) {
+        if(this.root == null) {
+            root = new Node(key, value);
+            root.left = null;
+            root.right = null;
+            size++;
+            return;
+        }
+        int cmp = this.root.key.compareTo(key);
+        if(cmp == 0) {
+            root.value = value;
+            return;
+        }
+        if(cmp > 0) {
+            this.root.left = putHelper(key, value, this.root.left);
+            size++;
+            return;
+        } else {
+            this.root.right = putHelper(key, value, this.root.right);
+            size++;
+            return;
+        }
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
+
+    /* Returns a Set view of the keys contained in this map. */
+    @Override
+    public Set<K> keySet() {
+        Set<K> keySet = new HashSet<>();
+        keySet.add(this.root.key);
+        HelperKeySet(keySet, this.root.right);
+        HelperKeySet(keySet, this.root.left);
+        return keySet;
+    }
+
+    private void HelperKeySet(Set<K> keySet, Node p) {
+        if(p == null) {
+            return;
+        }
+        keySet.add(p.key);
+        HelperKeySet(keySet, p.left);
+        HelperKeySet(keySet, p.right);
+    }
+
+    /** Removes KEY from the tree if present
+     *  returns VALUE removed,
+     *  null on failed removal.
+     */
+    @Override
+    public V remove(K key) {
+        if(get(key) == null) {
+            return null;
+        }
+        remove(key, this.root);
+        size--;
+        return get(key);
+    }
+
+    private Node FindBiggestInTheLeft(Node p) {
+        if(p == null) {
+            return null;
+        }
+        if(p.right == null) {
+            return p;
+        } else {
+            FindBiggestInTheLeft(p.right);
+        }
+        return p;
+    }
+
+    private Node remove(K key, Node p) {
+        int cmp = key.compareTo(p.key);
+        if(cmp > 0) {
+            p.right = remove(key, p.right);
+        } else if (cmp < 0) {
+            p.left = remove(key, p.left);
+        } else {
+            if(p.left == null) {
+                return p.right;
+            }
+            if(p.right == null) {
+                return p.left;
+            }
+            Node t = FindBiggestInTheLeft(p.left);
+            t.left = p.left;
+            t.right = DeleteMax(p.left);
+
+        }
+        return p;
+    }
+
+    private Node DeleteMax(Node p) {
+        if(p.right == null) {
+            return p.left;
+        }
+        p.right = DeleteMax(p.right);
+        return p;
+    }
+
+    /** Removes the key-value entry for the specified key only if it is
+     *  currently mapped to the specified value.  Returns the VALUE removed,
+     *  null on failed removal.
+     **/
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return keySet().iterator();
+    }
+}
